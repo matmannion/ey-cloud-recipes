586c42cb3da498a1865b6f5614111faf4e18127a
Mat Mannion <mat.mannion@gmail.com>
2014-07-26 11:35:18 +0100

    Tripwire install script for non-interactive installs

--- cookbooks/tripwire/attributes/default.rb
+++ cookbooks/tripwire/attributes/default.rb
@@ -1,1 +1,3 @@
 default[:tripwire_version] = "2.4.2.2"
+default[:tripwire_site_passphrase] = "c4V6X0i89oOP87V6QQZq01G37J48D7g7E2E30U5S"
+default[:tripwire_local_passphrase] = "5i9F17EXp8g89KP988z5535Ev5747y3mbqbgJ23j"
--- cookbooks/tripwire/recipes/default.rb
+++ cookbooks/tripwire/recipes/default.rb
@@ -22,3 +22,19 @@ template '/etc/tripwire/twpol.txt' do
 	group node[:owner_name]
 	source 'twpol.txt.erb'
 end
+
+# Install the twinstall.sh file.
+template '/etc/tripwire/twinstall.sh' do
+	owner node[:owner_name]
+	group node[:owner_name]
+	source 'twinstall.sh.erb'
+	mode 0755
+	backup false
+	action :create
+end
+
+#execute "plugin install #{name}" do
+#	cwd "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}"
+#	command "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/bin/plugin -install #{name}"
+#	not_if { File.directory?("/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/plugins/#{name}") }
+#end
