37db129bfa8ba893a4e7ee59610dd75afe245ea9
Mat Mannion <mat.mannion@gmail.com>
2014-07-26 15:41:08 +0100

    Use mktwpol to create twpol.txt instead of creating it by hand

--- cookbooks/tripwire/recipes/default.rb
+++ cookbooks/tripwire/recipes/default.rb
@@ -3,55 +3,77 @@
 # Recipe:: default
 #
 
-ey_cloud_report "Tripwire" do
-  message "configuring tripwire"
-end
+if ['solo', 'app', 'util', 'app_master'].include?(node[:instance_role])
+	ey_cloud_report "Tripwire" do
+	  message "configuring tripwire"
+	end
 
-include_recipe "tripwire::install"
+	include_recipe "tripwire::install"
 
-# Install the twcfg.txt file.
-template '/etc/tripwire/twcfg.txt' do
-	owner node[:owner_name]
-	group node[:owner_name]
-	source 'twcfg.txt.erb'
-end
+	# Install the twcfg.txt file.
+	template '/etc/tripwire/twcfg.txt' do
+		owner node[:owner_name]
+		group node[:owner_name]
+		source 'twcfg.txt.erb'
+	end
 
-# Install the twpol.txt file.
-template '/etc/tripwire/twpol.txt' do
-	owner node[:owner_name]
-	group node[:owner_name]
-	source 'twpol.txt.erb'
-end
+	# Use mktwpol to generate the twpol.txt policy file
+	remote_file "/tmp/mktwpol-0.2.4.tar.gz" do
+  		source "mktwpol-0.2.4.tar.gz"
+  		action :create_if_missing
+  		mode 0644
+	end
 
-# Install the twinstall.sh file.
-template '/etc/tripwire/twinstall.sh' do
-	owner node[:owner_name]
-	group node[:owner_name]
-	source 'twinstall.sh.erb'
-	variables(
-		:tripwire_site_passphrase => node[:tripwire_site_passphrase],
-		:tripwire_local_passphrase => node[:tripwire_local_passphrase]
-	)
-	mode 0755
-	backup false
-	action :create
-end
+	execute "install mktwpol" do
+  		command "tar -zxvf /tmp/mktwpol-0.2.4.tar.gz mktwpol-0.2.4/"
+  		cwd "/data/"
+	end
 
-# Run the install file to generate keys
-execute "generate tripwire keys" do
-	cwd "/etc/tripwire"
-	command "/etc/tripwire/twinstall.sh"
-end
+	# Create rules file
+	template '/data/mktwpol-0.2.4/mktwpol-engineyard.rules' do
+		owner root
+		group root
+		source 'mktwpol-engineyard.rules.erb'
+		variables(
+			:tripwire_emailto => node[:tripwire_emailto]
+		)
+		mode 0600
+	end
 
-# If the database hasn't been generated yet, generate it
-execute "generate tripwire database" do
-	cwd "/etc/tripwire"
-	command "/usr/sbin/tripwire --init -P #{node[:tripwire_local_passphrase]}"
-#/var/lib/tripwire/ip-172-31-42-172.twd
-end
+	# Create the twpol.txt file
+	execute "create tripwire policy file from rules" do
+		command "sudo /data/mktwpol-0.2.4/mktwpol-0.2.4.sh -R /data/mktwpol-0.2.4/mktwpol-engineyard.rules > /etc/tripwire/twpol.txt"
+		cwd "/etc/tripwire"
+	end
 
-# Sign the policy file
-execute "sign tripwire policy file" do
-	cwd "/etc/tripwire"
-	command "/usr/sbin/twadmin -m P -Q #{node[:tripwire_site_passphrase]} /etc/tripwire/twpol.txt"
+	# Install the twinstall.sh file.
+	template '/etc/tripwire/twinstall.sh' do
+		owner node[:owner_name]
+		group node[:owner_name]
+		source 'twinstall.sh.erb'
+		variables(
+			:tripwire_site_passphrase => node[:tripwire_site_passphrase],
+			:tripwire_local_passphrase => node[:tripwire_local_passphrase]
+		)
+		mode 0755
+	end
+
+	# Run the install file to generate keys
+	execute "generate tripwire keys" do
+		cwd "/etc/tripwire"
+		command "/etc/tripwire/twinstall.sh"
+	end
+
+	# If the database hasn't been generated yet, generate it
+	execute "generate tripwire database" do
+		cwd "/etc/tripwire"
+		command "/usr/sbin/tripwire --init -P #{node[:tripwire_local_passphrase]}"
+	#/var/lib/tripwire/ip-172-31-42-172.twd
+	end
+
+	# Sign the policy file
+	execute "sign tripwire policy file" do
+		cwd "/etc/tripwire"
+		command "/usr/sbin/twadmin -m P -Q #{node[:tripwire_site_passphrase]} /etc/tripwire/twpol.txt"
+	end
 end
