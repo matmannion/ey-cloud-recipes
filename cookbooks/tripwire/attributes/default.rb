11cc2af64815ecf9e6590dd1db6268716b17edd1
Mat Mannion <mat.mannion@gmail.com>
2014-07-26 18:19:05 +0100

    Set up a cron to run tripwire regularly

--- cookbooks/tripwire/attributes/default.rb
+++ cookbooks/tripwire/attributes/default.rb
@@ -2,3 +2,5 @@ default[:tripwire_version] = "2.4.2.2"
 default[:tripwire_site_passphrase] = "c4V6X0i89oOP87V6QQZq01G37J48D7g7E2E30U5S"
 default[:tripwire_local_passphrase] = "5i9F17EXp8g89KP988z5535Ev5747y3mbqbgJ23j"
 default[:tripwire_emailto] = "mat.mannion@gmail.com"
+default[:tripwire_cron_hour] = 3
+default[:tripwire_cron_minute] = 30
---
+++ cookbooks/tripwire/recipes/cron.rb
@@ -0,0 +1,11 @@
+ey_cloud_report "Tripwire" do
+  message "adding nightly run of Tripwire checks"
+end
+
+cron "tripwire report" do
+	user     "root" # needs to run as root
+	action   :create
+	minute   node[:tripwire_cron_minute]
+	hour     node[:tripwire_cron_hour]
+	command  "/usr/sbin/tripwire --check -e #{node[:tripwire_emailto]}"
+end
