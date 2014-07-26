fa03280d1f5591a1adb9949995a2f9ede1be6919
Mat Mannion <mat.mannion@gmail.com>
2014-07-26 13:14:27 +0100

    Set passphrase correctly and pass through

--- cookbooks/tripwire/recipes/default.rb
+++ cookbooks/tripwire/recipes/default.rb
@@ -28,13 +28,18 @@ template '/etc/tripwire/twinstall.sh' do
 	owner node[:owner_name]
 	group node[:owner_name]
 	source 'twinstall.sh.erb'
+	variables(
+		:tripwire_site_passphrase => node[:tripwire_site_passphrase],
+		:tripwire_local_passphrase => node[:tripwire_local_passphrase]
+	)
 	mode 0755
 	backup false
 	action :create
 end
 
-#execute "plugin install #{name}" do
-#	cwd "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}"
-#	command "/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/bin/plugin -install #{name}"
+# Run the install file to generate keys
+#execute "generate tripwire keys" do
+#	cwd "/etc/tripwire"
+#	command "/etc/tripwire/twinstall.sh"
 #	not_if { File.directory?("/usr/lib/elasticsearch-#{node[:elasticsearch_version]}/plugins/#{name}") }
 #end
